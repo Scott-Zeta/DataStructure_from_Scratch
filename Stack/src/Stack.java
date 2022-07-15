54e4ba34268dd0f10bebdeebcda8450bb90b3ffe
Scott-Zeta <wc070211@gmail.com>
2022-07-15 18:08:28 +0930

    stack finished and tested

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,3 +1,37 @@
-public class Stack {
-    
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class Stack <T> {
+   private LinkedList <T> list = new LinkedList<T>();
+
+   public Stack(){
+    //empty constructor
+   }
+
+   public Stack(T element){
+    //constructor with first element.
+    push(element);
+   }
+
+   public int size(){
+    return list.size();
+   }
+
+   public boolean isEmpty(){
+    return size() == 0;
+   }
+
+   public void push(T element){
+    list.addFirst(element);
+   }
+
+   public T pop(){
+    if(isEmpty()) throw new EmptyStackException();
+    return list.removeFirst();
+   }
+
+   public T peek(){
+    if(isEmpty()) throw new EmptyStackException();
+    return list.peekLast();
+   }
 }
--- Stack/src/Test.java
+++ Stack/src/Test.java
@@ -1,5 +1,13 @@
 public class Test {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Stack<String> stack1 = new Stack<String>("AAA");
+        System.out.println("current size: " + stack1.size());
+        stack1.push("BBB");
+        System.out.println("current size: " + stack1.size());
+        System.out.println("should be BBB:" + stack1.peek());
+        System.out.println("BBB been pop out:" + stack1.pop());
+        System.out.println("should be AAA:" + stack1.peek());
+        System.out.println("empty? should not:" + stack1.isEmpty());
+        System.out.println("Done");
     }
 }
