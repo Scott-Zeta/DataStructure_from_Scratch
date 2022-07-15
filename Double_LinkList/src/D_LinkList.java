d2cc23b0461b2382d526191aa865b1182399dd0e
Scott-Zeta <wc070211@gmail.com>
2022-07-15 13:37:53 +0930

    implement add function

--- Double_LinkList/src/D_LinkList.java
+++ Double_LinkList/src/D_LinkList.java
@@ -14,4 +14,40 @@ public class D_LinkList <T>{
             return data.toString();
         }
     }
+
+    private int size = 0;
+    private Node<T> head = null;
+    private Node<T> tail = null;
+
+    public void clear(){
+        Node<T> pinPoint = head;
+        while(pinPoint != null){
+            Node<T> nextStep = pinPoint.next;
+            pinPoint.data = null;
+            pinPoint.prev = null;
+            pinPoint.next = null;
+            pinPoint = nextStep;
+        }
+        head = tail = pinPoint = null;
+        size = 0;
+    }   
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+
+    public void add(T element){
+        if(isEmpty()){
+            head = tail = new Node<>(element, null, null);
+        }else{
+            tail.next = new Node<>(element, tail, null);
+            tail = tail.next;
+        }
+        size++;
+    }
+    
 }
