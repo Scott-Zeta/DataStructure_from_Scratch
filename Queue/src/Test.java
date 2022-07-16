df608149a64e8dd8cb813b7204df844deab6d4ce
Scott-Zeta <wc070211@gmail.com>
2022-07-16 17:53:15 +0930

    Tested

--- Queue/src/Test.java
+++ Queue/src/Test.java
@@ -1,5 +1,13 @@
 public class Test {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Queue<String> q1 = new Queue<>("Apple");
+        q1.eat("Banana");
+        System.out.println(q1.peek());
+        System.out.println(q1.poop());
+        System.out.println(q1.peek());
+        System.out.println(q1.size());
+        q1.poop();
+        System.out.println(q1.isEmpty());
+        System.out.println("Done");
     }
 }
