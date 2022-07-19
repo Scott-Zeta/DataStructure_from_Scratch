691929da1b2feb2889b4f6e8e6ff22ed99ef30eb
Scott-Zeta <wc070211@gmail.com>
2022-07-19 17:23:41 +0930

    finished the contains function

--- Binary_Search_Tree/src/B_Tree.java
+++ Binary_Search_Tree/src/B_Tree.java
@@ -14,4 +14,44 @@ public class B_Tree<T extends Comparable<T>> {
             this.right = right;
         }
     }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean contains(T element) {
+        return contains(element, root);
+    }
+
+    private boolean contains(T element, Node root) {
+        if (root == null) {
+            return false;
+        }
+
+        if (element.compareTo(root.data) < 0) {
+            return contains(element, root.left);
+        } else if (element.compareTo(root.data) > 0) {
+            return contains(element, root.right);
+        } else {
+            return true;
+        }
+    }
+
+    private Node add(T element, Node node) {
+        if (node == null) {
+            node = new Node(element, null, null);
+            // when no node in the tree, add as root.
+        } else {
+            if (element.compareTo(node.data) < 0) {
+                node.left = add(element, node.left); // return to its parent
+            } else {
+                node.right = add(element, node.right);
+            }
+        }
+        return node;
+    }
 }
