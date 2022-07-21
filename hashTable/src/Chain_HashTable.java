f84202e9665fd76c930dfb99c30050f6e61f0309
Scott-Zeta <wc070211@gmail.com>
2022-07-21 18:21:09 +0930

    not finished, did some addtional test

--- hashTable/src/Chain_HashTable.java
+++ hashTable/src/Chain_HashTable.java
@@ -11,43 +11,107 @@ class Entry<K, V> {
         this.hashCode = key.hashCode();
     }
 
-    public boolean equals(Entry<K,V> anotherEntry){
-        //if hash code are different, it must be different
-        if(hashCode != anotherEntry.hashCode){
+    public boolean equals(Entry<K, V> anotherEntry) {
+        // if hash code are different, it must be different
+        if (hashCode != anotherEntry.hashCode) {
             return false;
         }
-        //if hash code are same, might be the same, need to compare
+        // if hash code are same, might be the same, need to compare
         return key.equals(anotherEntry.key);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return key + "->" + value;
     }
 }
 
-public class Chain_HashTable<K,V> {
-    //load factor = number of entries / total size of array or table slot
+public class Chain_HashTable<K, V> {
+    // load factor = number of entries / total size of array or table slot
     // 0.5 load factor = half full, conllision relavent.
-    private static final int Deafault_Capacity = 3;
-    private static final double Deafault_Load_Factor = 0.75;
+    private static final int Default_Capacity = 3;
+    private static final double Default_Load_Factor = 0.75;
 
     private double maxLoadFactor;
     private int capacity;
     private int threshold;
     private int size;
-    private LinkedList<Entry<K,V>>[] slotArray; //the list contains entries with same slot
+    private LinkedList<Entry<K, V>>[] slotArray; // the list contains entries with same slot
 
-    public Chain_HashTable(int capacity, double maxLoadFactor){
-        if(capacity<0) throw new IllegalArgumentException("Illegal capacity");
-        //prevent it from negative, x/0 = infinite, sqrt(-1) = imaginary number
-        if(maxLoadFactor <=0 || Double.isNaN(maxLoadFactor) || Double.isInfinite(maxLoadFactor)) 
+    public Chain_HashTable(int capacity, double maxLoadFactor) {
+        if (capacity < 0)
+            throw new IllegalArgumentException("Illegal capacity");
+        // prevent it from negative, x/0 = infinite, sqrt(-1) = imaginary number
+        if (maxLoadFactor <= 0 || Double.isNaN(maxLoadFactor) || Double.isInfinite(maxLoadFactor))
             throw new IllegalArgumentException("Illegal maxLoadFactor");
         this.maxLoadFactor = maxLoadFactor;
-        this.capacity = Math.max(capacity,Deafault_Capacity);
-        //When over the threshold, like LoadFactor =0.75, 100 slots with 75 full, expand the hash table and do something else
-        threshold = (int)(this.capacity * maxLoadFactor);
-        //for each slot, a new linkedList for chain.
+        this.capacity = Math.max(capacity, Default_Capacity);
+        // When over the threshold, like LoadFactor =0.75, 100 slots with 75 full,
+        // expand the hash table and do something else
+        threshold = (int) (this.capacity * maxLoadFactor);
+        // for each slot, a new linkedList for chain.
         slotArray = new LinkedList[this.capacity];
+    }
+
+    public Chain_HashTable() {
+        this(Default_Capacity, Default_Load_Factor);
+    }
+
+    public Chain_HashTable(int capacity) {
+        this(capacity, Default_Load_Factor);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void clear() {
+        // empty all slot linked lists
+        Arrays.fill(slotArray, null);
+        size = 0;
+    }
+
+    private int normalizeIndex(int keyHash) {
+        return (keyHash & 0x7FFFFFFF) % capacity;
+        // strip off the negative number by bit operation &, then get the index of the
+        // slot
+    }
+
+    private Entry<K, V> searchEntryInSlot(int slotIndex, K key) {
+        // see if the key exsist in the particular slot.
+        if (key == null) {
+            return null;
+        }
+        LinkedList<Entry<K, V>> slot = slotArray[slotIndex];
+        for (Entry<K, V> entry : slot) {
+            if (entry.key.equals(key)) {
+                return entry;
+            }
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        if (key == null) {
+            return null;
+        }
+        int slotindex = normalizeIndex(key.hashCode());
+        return removeInSlot(slotindex, key);
+    }
+
+    private V removeInSlot(int slotIndex, K key){
+        Entry<K,V> entry = searchEntryInSlot(slotIndex, key);
+        if(entry != null){
+            V value = entry.value;
+            slotArray[slotIndex].remove(entry);
+            size--;
+            return value;
+        }else{
+            return null;
+        }
     }
 }
--- hashTable/src/Test.java
+++ hashTable/src/Test.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 public class Test {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        LinkedList<String>[] listArray = new LinkedList[10];
+        listArray[1] = new LinkedList<>();
+        listArray[1].add("Apple");
+        System.out.println(listArray[1].contains("Apple"));
+        
+        LinkedList<String> copyList = listArray[1];
+        copyList.remove("Apple");
+        System.out.println(copyList.size());
+        System.out.println(listArray[1].size());
+        System.out.println(listArray[1].contains("Apple")); 
+        System.out.println("Done");
     }
 }
